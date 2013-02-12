e5933960d13cd1d3cd0e1a2ade3162743b89b776
Iñaki Baz Castillo <ibc@aliax.net>
2013-02-12 02:05:41 +0100

    Added qunitjs based test unit (for now a parser test) and integrate it in grunt.js.

--- BUILDING.md
+++ BUILDING.md
@@ -1,22 +1,31 @@
 ## What you need to build JsSIP
 
-You just need to have `Node.js`/`npm` and `git` installed.
+You just need to have [Node.js](http://nodejs.org/), [Git](http://git-scm.com/) and [PhantomJS](http://phantomjs.org/) installed.
+
+### Node.js
 
 * [Install Node.js via package manager](https://github.com/joyent/node/wiki/Installing-Node.js-via-package-manager)
-* [Install Node.js from source](http://nodejs.org)
+* [Install Node.js from sources](http://nodejs.org)
+
+### Git
+
 * [Install Git](http://git-scm.com/book/en/Getting-Started-Installing-Git)
 
-**NOTE:** `npm` comes with `node` now.
+
+### PhantomJS
+
+* [Install PhantomJS](http://phantomjs.org/download.html)
+* In modern Debian/Ubuntu systems PhantomJS can be installed via `apt-get install phantomjs`
 
 
 ## How to build JsSIP
 
-Clone a copy of the main JsSIP git repo by running:
+Clone a copy of the main JsSIP git repository by running:
 ```
 git clone https://github.com/versatica/JsSIP.git
 ```
 
-Enter the directory and install the Node dependencies:
+Enter the directory and install the Node.js dependencies:
 ```
 cd JsSIP && npm install
 ```
@@ -26,9 +35,9 @@ Make sure you have `grunt` installed by testing:
 grunt -version
 ```
 
-Finally, to get a complete version of JsSIP, type the following:
+Finally, to run the test units and get a complete version of JsSIP, type the following:
 ```
 grunt
 ```
 
-The built version of JsSIP will be available in the `dist/` subdirectory in both flavors: normal and minified. Both linted (with JSHint).
+The built version of JsSIP will be available in the `dist/` subdirectory in both flavors: normal and minified. Both linted (with [JSHint](http://www.jshint.com/)).
---
+++ qunitjs/javascript/test-parser.js
@@ -0,0 +1,107 @@
+test('Parse URI', function() {
+  var data = 'SIP:%61liCE@versaTICA.Com:6060;TRansport=TCp;Foo=123;baz?X-Header-1=AaA1&X-Header-2=BbB&x-header-1=AAA2';
+  var uri = JsSIP.Utils.parseURI(data);
+
+  // Parsed data.
+  ok(uri instanceof(JsSIP.URI));
+  strictEqual(uri.scheme, 'sip');
+  strictEqual(uri.user, 'aliCE');
+  strictEqual(uri.host, 'versatica.com');
+  strictEqual(uri.port, 6060);
+  strictEqual(uri.hasParam('transport'), true);
+  strictEqual(uri.hasParam('nooo'), false);
+  strictEqual(uri.getParam('transport'), 'tcp');
+  strictEqual(uri.getParam('foo'), '123');
+  strictEqual(uri.getParam('baz'), null);
+  strictEqual(uri.getParam('nooo'), undefined);
+  deepEqual(uri.getHeader('x-header-1'), ['AaA1', 'AAA2']);
+  deepEqual(uri.getHeader('X-HEADER-2'), ['BbB']);
+  strictEqual(uri.getHeader('nooo'), undefined);
+  strictEqual(uri.toString(), 'sip:aliCE@versatica.com:6060;transport=tcp;foo=123;baz?X-Header-1=AaA1&X-Header-1=AAA2&X-Header-2=BbB');
+  strictEqual(uri.toAor(), 'sip:aliCE@versatica.com');
+
+  // Alter data.
+  uri.user = "Iñaki";
+  strictEqual(uri.user, 'Iñaki');
+  strictEqual(uri.deleteParam('foo'), '123');
+  strictEqual(uri.deleteHeader('x-header-1'), ['AaA1', 'AAA2']);
+  strictEqual(uri.toString(), 'sip:I%C3%B1aki@versatica.com:6060;transport=tcp;baz?X-Header-2=BbB');
+  strictEqual(uri.toAor(), 'sip:I%C3%B1aki@versatica.com');
+  uri.clearParams();
+  uri.clearHeaders();
+  uri.port = null;
+  strictEqual(uri.toString(), 'sip:I%C3%B1aki@versatica.com');
+  strictEqual(uri.toAor(), 'sip:I%C3%B1aki@versatica.com');
+});
+
+
+test('Parse multiple Contact', function() {
+  var data = '"Iñaki @ł€" <SIP:IBC@ALIAX.net;Transport=WS>;+sip.Instance="abCD", sip:bob@biloxi.COM;headerParam, <sip:DOMAIN.com:5>';
+  var contacts = JsSIP.Grammar.parse(data, 'Contact');
+
+  ok(contacts instanceof(Array));
+  strictEqual(contacts.length, 3);
+  var c1 = contacts[0].parsed;
+  var c2 = contacts[1].parsed;
+  var c3 = contacts[2].parsed;
+
+  // Parsed data.
+  ok(c1 instanceof(JsSIP.NameAddrHeader));
+  strictEqual(c1.display_name, 'Iñaki @ł€');
+  strictEqual(c1.hasParam('+sip.instance'), true);
+  strictEqual(c1.hasParam('nooo'), false);
+  strictEqual(c1.getParam('+SIP.instance'), '"abcd"');
+  strictEqual(c1.getParam('nooo'), undefined);
+  ok(c1.uri instanceof(JsSIP.URI));
+  strictEqual(c1.uri.scheme, 'sip');
+  strictEqual(c1.uri.user, 'IBC');
+  strictEqual(c1.uri.host, 'aliax.net');
+  strictEqual(c1.uri.port, undefined);
+  strictEqual(c1.uri.getParam('transport'), 'ws');
+  strictEqual(c1.uri.getParam('foo'), undefined);
+  strictEqual(c1.uri.getHeader('X-Header'), undefined);
+  strictEqual(c1.toString(), '"Iñaki @ł€" <sip:IBC@aliax.net;transport=ws>;+sip.instance="abcd"');
+
+  // Alter data.
+  c1.display_name = "€€€";
+  strictEqual(c1.display_name, '€€€');
+  c1.setParam('+sip.instance', '"zxCV"');
+  strictEqual(c1.getParam('+SIP.instance'), '"zxcv"');
+  c1.setParam('New-Param', null);
+  strictEqual(c1.hasParam('NEW-param'), true);
+  c1.uri.setParam('New-Param', null);
+  strictEqual(c1.toString(), '"€€€" <sip:IBC@aliax.net;transport=ws;new-param>;+sip.instance="zxcv";new-param');
+
+  // Parsed data.
+  ok(c2 instanceof(JsSIP.NameAddrHeader));
+  strictEqual(c2.display_name, undefined);
+  strictEqual(c2.hasParam('HEADERPARAM'), true);
+  ok(c2.uri instanceof(JsSIP.URI));
+  strictEqual(c2.uri.scheme, 'sip');
+  strictEqual(c2.uri.user, 'bob');
+  strictEqual(c2.uri.host, 'biloxi.com');
+  strictEqual(c2.uri.port, undefined);
+  strictEqual(c2.uri.hasParam('headerParam'), false);
+  strictEqual(c2.toString(), '<sip:bob@biloxi.com>;headerparam');
+
+  // Alter data.
+  c2.display_name = "@ł€ĸłæß";
+  strictEqual(c2.toString(), '"@ł€ĸłæß" <sip:bob@biloxi.com>;headerparam');
+
+  // Parsed data.
+  ok(c3 instanceof(JsSIP.NameAddrHeader));
+  strictEqual(c3.display_name, undefined);
+  ok(c3.uri instanceof(JsSIP.URI));
+  strictEqual(c3.uri.scheme, 'sip');
+  strictEqual(c3.uri.user, undefined);
+  strictEqual(c3.uri.host, 'domain.com');
+  strictEqual(c3.uri.port, 5);
+  strictEqual(c3.uri.hasParam('nooo'), false);
+  strictEqual(c3.toString(), '<sip:domain.com:5>');
+
+  // Alter data.
+  c3.uri.setParam('newUriParam', 'zxCV');
+  c3.setParam('newHeaderParam', 'zxCV');
+  strictEqual(c3.toString(), '<sip:domain.com:5;newuriparam=zxcv>;newheaderparam=zxcv');
+});
+
