ff5f790a3bd6df36ad54b1de0ecadfa5cf362848
José Luis Millán <jmillan@aliax.net>
2013-09-11 18:29:57 +0200

    Don't force 'gcc' as compressor type in Grammar compression
    - Allow specifying a compressor type among those supported by node-minify

--- src/Grammar/minify.js
+++ src/Grammar/minify.js
@@ -1,11 +1,22 @@
 var compressor = require('node-minify');
 
-// Use GCC Google Closure Compiler
-new compressor.minify({
-    type: 'gcc',
-    fileIn: 'dist/Grammar.js',
-    fileOut: 'dist/Grammar.min.js',
-    callback: function(err){
-      console.log(err);
-    }
-});
+var compressors = ['gcc','yui-js','uglifyjs','sqwish'];
+
+// Use GCC Google Closure Compiler by default
+var compressorType = process.argv[2] || 'gcc';
+
+if (compressors.indexOf(compressorType) === -1 ) {
+  console.log(
+    'Wrong compressor type specified. ' +
+    'The supported compressor types are: '+ compressors.join(', ')
+  );
+} else {
+  new compressor.minify({
+      type: compressorType,
+      fileIn: 'dist/Grammar.js',
+      fileOut: 'dist/Grammar.min.js',
+      callback: function(err){
+        console.log(err);
+      }
+  });
+}
