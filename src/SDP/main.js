5179d2a90d6b4d83df3b106cf2c5fd58b40e99e5
José Luis Millán <jmillan@aliax.net>
2013-12-03 11:10:26 +0100

    Hold/Mute implementation

---
+++ src/SDP/main.js
@@ -0,0 +1,19 @@
+/**
+ * @fileoverview SDP Parser
+ *
+ * https://github.com/clux/sdp-transform
+ * 
+ */
+
+(function(JsSIP) {
+
+  var parser = require('sdp-transform');
+
+  JsSIP.Parser.parseSDP = parser.parse;
+  JsSIP.Parser.writeSDP = parser.write;
+  JsSIP.Parser.parseFmtpConfig = parser.parseFmtpConfig;
+  JsSIP.Parser.parsePayloads = parser.parsePayloads;
+  JsSIP.Parser.parseRemoteCandidates = parser.parseRemoteCandidates;
+
+}(JsSIP));
+
