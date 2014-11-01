aafc087d1e282b32762561960a6226e625a8ec33
José Luis Millán <jmillan@aliax.net>
2014-11-01 12:28:20 +0100

    RTCSession: add custom 'data' attribute to UA.call() and RTCSession.answer() 'options' parameter

--- src/RTCSession.js
+++ src/RTCSession.js
@@ -349,6 +349,7 @@ RTCSession.prototype.answer = function(options) {
       self.failed('system', null, JsSIP.C.causes.WEBRTC_ERROR);
     };
 
+  this.data = options.data || {};
 
   // Check Session Direction and Status
   if (this.direction !== 'incoming') {
@@ -932,6 +933,8 @@ RTCSession.prototype.connect = function(target, options) {
     RTCOfferConstraints = options.RTCOfferConstraints || {},
     stun_servers = options.stun_servers || null,
     turn_servers = options.turn_servers || null;
+
+  this.data = options.data || {};
 
   if (stun_servers) {
     iceServers = JsSIP.UA.configuration_check.optional.stun_servers(stun_servers);
