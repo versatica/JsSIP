3d829c8bf913f042750ee5c52c5d1ad9bf282497
phweb <pheonix.khmao@gmail.com>
2020-06-01 21:25:08 +0500

    Typescript typings (#627)
    Co-authored-by: Vitaliy Yakovenko <yakovenkovv@yandex-team.ru>

---
+++ lib/JsSIP.d.ts
@@ -0,0 +1,14 @@
+import {Debug} from 'debug'
+
+export * as C from './Constants'
+export * as Exceptions from './Exceptions'
+export * as Utils from './Utils'
+export {UA} from './UA'
+export {URI} from './URI'
+export {NameAddrHeader} from './NameAddrHeader'
+export {WebSocketInterface} from './WebSocketInterface'
+export * as Grammar from './Grammar'
+
+export const debug: Debug
+export const name: string
+export const version: string
---
+++ lib/NameAddrHeader.d.ts
@@ -0,0 +1,27 @@
+import {Parameters, URI} from './URI'
+import {Grammar} from './Grammar'
+
+export class NameAddrHeader {
+  get display_name(): string;
+  set display_name(value: string);
+
+  get uri(): URI;
+
+  constructor(uri: URI, display_name?: string, parameters?: Parameters);
+
+  setParam(key: string, value?: string): void;
+
+  getParam<T = any>(key: string): T;
+
+  hasParam(key: string): boolean;
+
+  deleteParam(key: string): void;
+
+  clearParams(): void;
+
+  clone(): this;
+
+  toString(): string;
+
+  static parse(uri: string): Grammar | undefined;
+}
