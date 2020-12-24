f8a18facad3f49c7a19f561d18a966eda2d86fa2
Krivega Dmitriy <mr.krivega@gmail.com>
2020-12-24 18:25:58 +0300

    release 3.10.0

--- dist/@krivega/jssip.js
+++ dist/@krivega/jssip.js
@@ -1,5 +1,5 @@
 /*
- * JsSIP v3.9.0
+ * JsSIP v3.10.0
  * the Javascript SIP library
  * Copyright: 2012-2020 José Luis Millán <jmillan@aliax.net> (https://github.com/jmillan)
  * Homepage: https://jssip.net
@@ -28045,7 +28045,7 @@ module.exports={
   "name": "@krivega/jssip",
   "title": "JsSIP",
   "description": "the Javascript SIP library",
-  "version": "3.9.0",
+  "version": "3.10.0",
   "homepage": "https://jssip.net",
   "author": "José Luis Millán <jmillan@aliax.net> (https://github.com/jmillan)",
   "contributors": [
