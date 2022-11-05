cd9ca51b8919083591f073ebc00ac3318fd2a147
stefang42 <73243114+stefang42@users.noreply.github.com>
2022-11-05 23:02:19 +0100

    New UA config param for global extra headers (#774)
    New UA configuration param "extra_headers"

--- lib/UA.d.ts
+++ lib/UA.d.ts
@@ -43,6 +43,7 @@ export interface UAConfiguration {
   registrar_server?: string;
   use_preloaded_route?: boolean;
   user_agent?: string;
+  extra_headers?: string[];
 }
 
 export interface IncomingRTCSessionEvent {
