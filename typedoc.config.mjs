db018e83f5875f664caef59b5523c77765afed56
José Luis Millán <jmillan@aliax.net>
2026-02-04 10:11:02 +0100

    typedoc config

---
+++ typedoc.config.mjs
@@ -0,0 +1,33 @@
+/**
+ * Configuration for Typedoc.
+ */
+
+/** @type {Partial<import('typedoc').TypeDocOptions>} */
+const config = {
+	entryPoints: ['src/JsSIP.ts'],
+	out: 'docs',
+	skipErrorChecking: false,
+	exclude: ['src/**/*.d.ts', 'src/test/**/test-*.ts'],
+	excludePrivate: true,
+	excludeProtected: true,
+	excludeNotDocumented: true,
+	excludeInternal: true,
+	excludeExternals: true,
+	includeVersion: true,
+	gitRemote: 'origin',
+	hideGenerator: false,
+	treatWarningsAsErrors: true,
+	cacheBust: true,
+	categorizeByGroup: false,
+	categoryOrder: ['Config', 'UA', 'RTCSession', '*'],
+	searchInComments: true,
+	readme: 'README.md',
+	projectDocuments: ['README.md', 'LICENSE.md'],
+	navigationLinks: {
+		GitHub: 'https://github.com/versatica/jssip',
+		NPM: 'https://www.npmjs.com/package/jssip',
+	},
+	customCss: './docs-assets/custom-styles.css',
+};
+
+export default config;
